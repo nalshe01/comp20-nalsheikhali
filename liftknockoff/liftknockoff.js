980c37a1a35ae5ff00ffa1ce0d0ce82fc4490dc6
Noaf Alsheikh-Ali <noafalsheikh-ali@Noafs-MacBook-Pro.local>
2019-03-04 16:32:16 -0500

    function to render vehicle markers has been added - not accounting for w-mobile or distance calculations

--- liftknockoff/liftknockoff.js
+++ liftknockoff/liftknockoff.js
@@ -14,20 +14,18 @@
             function init() {
                 map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
                 getMyLocation();
-                google.maps.event.addListener(marker, 'click', function() {
-                    var site = "https://hans-moleman.herokuapp.com/rides";
-                    request.open("POST", site, true);
-                    console.log("request opened");
-                    request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-                    console.log("set request header");
-                    request.onreadystatechange = function() {
+                request.open("POST", "https://hans-moleman.herokuapp.com/rides", true);
+                request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+                request.onreadystatechange = function() {
                     if (request.readyState == 4 && request.status == 200) {
+                        console.log("Got the data back!");
                         data = request.responseText;
-                        location = JSON.parse(data);
+                        console.log(data);
+                        vehicles = JSON.parse(data);
+                        vehicleMarkers();
                     }
-                    };
-                    request.send("username=Xt7n67RP&lat=" + posLat + "&lng=" + posLng);
-                });
+		        };
+                request.send("username=Xt7n67RP&lat=" + posLat + "&lng=" + posLng);
             }
 
             function getMyLocation() {
@@ -36,6 +34,7 @@
                         posLat = position.coords.latitude;
                         posLng = position.coords.longitude;
                         renderMap();
+                        
                     });
                 }
                 else {
@@ -55,6 +54,24 @@
                     infoWindow.setContent(marker.title);
                     infoWindow.open(map, marker);
                 });
+            }
+
+            function vehicleMarkers() {
+                var i;
+                var vMarker;
+                var vIWindow;
+                for (i = 0; i < vehicles.length; i++) {
+                    vMarker = new google.maps.Marker({
+                        position: new google.maps.LatLng(vehicles[i].lat, vehicles[i].lng),
+                        title: "vehicles[i].username",
+                        icon: "https://tuftsdev.github.io/WebProgramming/assignments/car.png"
+                    });
+                    vIWindow = new google.maps.InfoWindow();
+                    google.maps.event.addListener(vMarker, 'click', function() {
+                        vIWindow.setContent(vMarker.title);
+                        vIWindow.open(map, vMarker);
+                    });
+                }
             }
 
             
